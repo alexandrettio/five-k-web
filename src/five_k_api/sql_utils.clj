38953c3af597f877409d663507dc74cb335451d0
Aleksandr Sher <aleksandrsher@gmail.com>
2016-10-02 20:30:31 +0300

    plus one variant

--- src/five_k_api/sql_utils.clj
+++ src/five_k_api/sql_utils.clj
@@ -6,7 +6,7 @@
   [s]
   (s/replace s #"-" "_"))
 
-(defn create-table
+(defn create-table-sql
   [table-name columns-def]
   (jdbc/create-table-ddl
    (name table-name)
@@ -23,8 +23,11 @@
   (->>
    db-spec
    (sort-by #(-> % second :order))
-   (map (fn [[n c]] [n (:columns c)]))
-   (map (partial apply create-table))
+   ;;(map (fn [[n c]] [n (:columns c)]))
+   ;; and another variant =)
+   ;; more clear explanation of what we do
+   (map #(update-in % [1] :columns))
+   (map (partial apply create-table-sql))
    (run-ddls! db)))
 
 (defn drop-scheme!
